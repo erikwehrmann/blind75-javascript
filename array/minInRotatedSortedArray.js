0e53460804b245ccf4ee78d14a7fae0c0b9b47ce
Erik Wehrmann <erik.wehrmann@gmail.com>
2023-03-07 10:43:15 -0500

    fixed for case edges

--- array/minInRotatedSortedArray.js
+++ array/minInRotatedSortedArray.js
@@ -3,14 +3,30 @@ function findMin(nums) {
   let right = nums.length - 1;
   while (left < right) {
     mid = Math.floor((right + left) / 2);
-    if (nums[mid] < nums[left] && nums[mid] < nums[right]) {
-      return nums[mid];
+    // console.log(nums[left], nums[mid], nums[right]);
+    if (nums[mid - 1] > nums[mid]) return nums[mid];
+    if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
+    if (right - left === 1) {
+      if (nums[right] > nums[left]) return nums[left];
+      if (nums[right] < nums[left]) return nums[right];
     } else if (nums[mid] > nums[right]) {
-      left = mid + 1;
+      left = mid;
     } else {
       right = mid;
     }
   }
 }
 
-console.log(findMin([4, 5, 6, 7, 8, 1, 2, 3]));
+// console.log(findMin([0, 1, 2, 3, 4, 5, 6]), 0);
+// console.log(findMin([1, 2, 3, 4, 5, 6, 0]), 0);
+// console.log(findMin([2, 3, 4, 5, 6, 0, 1]), 0);
+// console.log(findMin([3, 4, 5, 6, 0, 1, 2]), 0);
+// console.log(findMin([4, 5, 6, 0, 1, 2, 3]), 0);
+// console.log(findMin([5, 6, 0, 1, 2, 3, 4]), 0);
+// console.log(findMin([6, 0, 1, 2, 3, 4, 5]), 0);
+// console.log(findMin([0, 1, 2, 3, 4, 5]), 0);
+// console.log(findMin([1, 2, 3, 4, 5, 0]), 0);
+// console.log(findMin([2, 3, 4, 5, 0, 1]), 0);
+// console.log(findMin([3, 4, 5, 0, 1, 2]), 0);
+// console.log(findMin([4, 5, 0, 1, 2, 3]), 0);
+// console.log(findMin([5, 0, 1, 2, 3, 4]), 0);
